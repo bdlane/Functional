42eba2b95389e9b003e4b853d18922692dc6b33c
bdlane <brendan@bdlane.com>
2019-10-26 17:49:39 +0100

    Add BeEmpty, BeSomething assertion extensions.

---
+++ Functional.FluentAssertions.Tests/MaybeAssertionsTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Functional.FluentAssertions.Tests
+{
+    public class MaybeAssertionsTests
+    {
+        [Fact]
+        public void CanAssertAMaybeIsEmpty()
+        {
+            // Arrange
+            var subject = new Nothing<object>();
+
+            // Act
+            Action act = () => subject.Should().BeEmpty();
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void AssertionFailsWhenAssertingAFilledMaybeIsEmpty()
+        {
+            // Arrange
+            var subject = new Just<object>(new object());
+
+            // Act
+            Action act = () => subject.Should().BeEmpty();
+
+            // Assert
+            act.Should().Throw<XunitException>("Expected subject to be empty, but it was something.");
+        }
+
+        [Fact]
+        public void AssertionFailsWhenAssertingAnEmptyMaybeIsFilled()
+        {
+            // Arrange
+            var subject = new Nothing<object>();
+
+            // Act
+            Action act = () => subject.Should().BeSomething();
+
+            // Assert
+            act.Should().Throw<XunitException>("Expected subject to be something, but it was empty.");
+        }
+
+        [Fact]
+        public void CanAssertAMabyeIsFilled()
+        {
+            // Arrange
+            var value = Guid.NewGuid();
+            var subject = new Just<Guid>(value);
+
+            // Act
+            Func<Guid> fun = () => subject.Should().BeSomething().Subject;
+
+            // Assert
+            fun.Should().NotThrow().Which.Should().Be(value);
+        }
+    }
+}
---
+++ Functional.FluentAssertions/MaybeAssertionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+using System;
+using System.Linq;
+
+namespace Functional.FluentAssertions
+{
+    public static class MaybeAssertionExtensions
+    {
+        public static MaybeAssertions<T> Should<T>(this IMaybe<T> maybe)
+        {
+            if (maybe is null)
+            {
+                throw new ArgumentNullException(nameof(maybe));
+            }
+
+            return new MaybeAssertions<T>(maybe);
+        }
+    }
+
+    // Inheriting from ObjectAssertions means we get BeEquivalentTo... etc. for free.
+    // Do we want all the methods on ObjectAssertions?
+    public class MaybeAssertions<T> : ObjectAssertions
+    {
+        public MaybeAssertions(IMaybe<T> maybe) : base(maybe)
+        {
+            if (maybe is null)
+            {
+                throw new ArgumentNullException(nameof(maybe));
+            }
+
+            Subject = maybe;
+        }
+
+        public new IMaybe<T> Subject { get; protected set; }
+
+        protected override string Identifier => "maybe";
+
+        public AndConstraint<MaybeAssertions<T>> BeEmpty(string because = "", params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(Subject.Match(true, _ => false))
+                .FailWith("Expected {context:subject} to be empty, but it was filled.");
+
+            return new AndConstraint<MaybeAssertions<T>>(this);
+        }
+
+        public AndWhichConstraint<MaybeAssertions<T>, T> BeSomething(string because = "", params object[] becauseArgs)
+        {
+            var value = Subject.Match(Enumerable.Empty<T>(), v => new T[] { v });
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(value.Any())
+                .FailWith("Expected {context:subject} to be something, but it was empty.");
+
+            return new AndWhichConstraint<MaybeAssertions<T>, T>(this, value.Single());
+        }
+    }
+}
