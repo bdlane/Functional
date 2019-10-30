b50c03678ba4af2b4cdeda01e4458dc0cfa2afff
bdlane <brendan@bdlane.com>
2019-10-30 19:48:33 +0000

    Add a MaybeSpecimenBuilder for AutoFixture.

---
+++ Functional.AutoFixture.Tests/MaybeSpecimenBuilderTests.cs
@@ -0,0 +1,32 @@
+using AutoFixture;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Functional.AutoFixture.Tests
+{
+    public class MaybeSpecimenBuilderTests
+    {
+        [Fact]
+        public void CannotCreateAMaybeFixtureWithoutCustomSpecimenBuilder()
+        {
+            var fixture = new Fixture();
+
+            Action actual = () => fixture.Create<IMaybe<object>>();
+
+            actual.Should().Throw<ObjectCreationException>();
+        }
+
+        [Fact]
+        public void CanCreateAMaybeFixtureWithCustomSpecimenBuilder()
+        {
+            var fixture = new Fixture();
+
+            fixture.Customizations.Add(new MaybeSpecimenBuilder());
+
+            var actual = fixture.Create<IMaybe<object>>();
+
+            actual.Should().BeAssignableTo<Just<object>>();
+        }
+    }
+}
---
+++ Functional.AutoFixture/MaybeSpecimenBuilder.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using System;
+using System.Linq;
+
+namespace Functional.AutoFixture
+{
+    public class MaybeSpecimenBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (!(request is Type t))
+            {
+                return new NoSpecimen();
+            }
+
+            if (!t.IsInterface || !t.IsGenericType || t.GetGenericTypeDefinition() != typeof(IMaybe<>))
+            {
+                return new NoSpecimen();
+            }
+
+            var containedType = t.GetGenericArguments().First();
+            var justType = typeof(Just<>).MakeGenericType(containedType);
+
+            var obj = context.Resolve(justType);
+
+            return obj;
+        }
+    }
+}
